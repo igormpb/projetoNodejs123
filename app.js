cea1475f404205b3181ec5d933359a791d0d7c66
igorBarros <igor.mpb@hotmail.com>
2020-02-08 02:27:54 -0300

    bd+categoria __funcionando :)

--- app.js
+++ app.js
@@ -6,24 +6,27 @@ const admin = require('./routes/admin');
 const path = require('path');
 const app = express();
 
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/blogapp").then(() => {
+    console.log("servidor rodando")
+}).catch((err) => {
+    console.log(err)
+})
 
 // template engine 
-app.engine('handlebars',handlebars({defaultLayout: 'main'}));
-app.set('view engine','handlebars');
-//
-
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 //body parser 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //public 
-app.use(express.static(path.join(__dirname + 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // rotas
 app.use('/admin', admin);
 
 
 
 // servidor
-app.listen(8081,()=>{
-    console.log('open server!')
-})
+app.listen(3003, () => {
+    console.log('open server!');
+});
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require("../models/Categoria")
+const Categoria =  mongoose.model("categorias");
 
 router.get('/',function(req,res){
-    res.render('index')
+    res.render('admin/index')
 })
-
+router.get('/categoria',(req,res)=>{
+    res.render('admin/categoria')
+})
+router.get('/categoria/add',(req,res)=>{
+    res.render('admin/addCategoria')
+})
 
 router.get('/posts',function(req,res){
     res.render('posts')
+})
+
+router.post('/categorias/nova',function(req,res){
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug:req.body.slug
+    }
+   new Categoria(novaCategoria).save().then(()=>{
+       console.log('registrado!')
+   }).catch((err)=>{
+       console.log(`houve um erro ${err}`)
+   })
 })
 
 module.exports = router;
